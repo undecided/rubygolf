b626d39320ab648680a1ac67390f5a8f18fb02c0
Matthew Bennett <matthew@eshopworks.co.uk>
2011-07-06 15:44:35 +0100

    I have a plan...

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,45 +1,42 @@
 class Golf
   class<<self
-    def hole1(j)
-      j.inject(1,:*)
+    def hole1 j
+      j.inject 1,:*
     end
     
-    def hole2(l)
-      l.split.sort {|a,b| a[1]<=>b[1]}.join(' ')
+    def hole2 l
+      l.split.sort {|a,b| a[1]<=>b[1]}.join ' '
     end
     
-    def hole3(k)
-      hole1(2..k)
+    def hole3 k
+      hole1 2..k
     end
     
-    def hole4(j)
-      j.map{|m| m.sub(/(man.+)/,'hat(\1)')}.
-          map{|m| m.sub(/(dog.+)\)/,'\1(bone))')}.
-          map{|m| m.sub('cat','dead')}
+    def hole4 j
+      j.map{|m| m.sub /(ma.+)/,'hat(\1)' }.
+          map{|m| m.sub /(do.+)\)/,'\1(bone))' }.
+          map{|m| m.sub 'cat','dead' }
     end
     
-    def hole5(j)
+    def hole5 j
       (1..j.size).map{|n| j.each_cons(n).to_a}.flatten(1)
     end
     
-    def hole6(k)0
-      (1..k).map{|n| n%15==0 ? 'fizzbuzz' : (n%3==0 ? 'fizz' : (n%5==0 ? 'buzz' : n))}
+    def hole6 k
+      (1..k).map{|n| n%15==0 ? 'fizzbuzz' : n%3==0 ? 'fizz' : n%5==0 ? 'buzz' : n}
     end
     
-    def hole7(a)
-      idxs = []
-      b = a
-      c = b.each_cons(1).to_a
+    def hole7 c
       d = []
-      c.each_with_index { |n,i| (d[0] && d.last.last == n[0] - 1) ? d.last << n[0] : d << [n[0]] }
+      c.map { |n| d[0] && d.last.last == n - 1 ? d.last << n : d << [n] }
       d.map { |n| n.size > 1 ? "#{n[0]}-#{n.last}" : n.to_s}
     end
 
-    def hole8(k)
-      (0..k-1).map{|n|f(n)}
+    def hole8 k
+      (0..k-1).map{|n|f n }
     end
     
-    def f(v)
+    def f v
       v < 2 ? 1 : f(v-1) + f(v-2)
     end
   end
